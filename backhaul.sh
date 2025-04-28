0288a91f6cbbec76ceb9be7141665eb202473407
MRDindar <rezadindar1381@gmail.com>
2025-04-28 23:22:42 +0330

    Update backhaul.sh

--- backhaul.sh
+++ backhaul.sh
@@ -130,10 +130,10 @@ download_and_extract_backhaul() {
     ARCH=$(uname -m)
     case "$ARCH" in
         x86_64)
-            DOWNLOAD_URL="https://raw.githubusercontent.com/wafflenoodle/zenith-stash/refs/heads/main/backhaul_amd64.tar.gz"
+            DOWNLOAD_URL="https://raw.githubusercontent.com/rezadindar/backhaul/refs/heads/main/backhaul_amd64.tar.gz"
             ;;
         arm64|aarch64)
-            DOWNLOAD_URL="https://raw.githubusercontent.com/wafflenoodle/zenith-stash/refs/heads/main/backhaul_arm64.tar.gz"
+            DOWNLOAD_URL="https://raw.githubusercontent.com/rezadindar/backhaul/refs/heads/main/backhaul_arm64.tar.gz"
             ;;
         *)
             echo -e "${RED}Unsupported architecture: $ARCH.${NC}"
@@ -1808,7 +1808,7 @@ update_script(){
 # Define the destination path
 DEST_DIR="/usr/bin/"
 BACKHAUL_SCRIPT="backhaul"
-SCRIPT_URL="https://raw.githubusercontent.com/wafflenoodle/zenith-stash/refs/heads/main/backhaul.sh"
+SCRIPT_URL="https://raw.githubusercontent.com/rezadindar/backhaul/refs/heads/main/backhaul.sh"
 
 echo
 # Check if backhaul.sh exists in /bin/bash
